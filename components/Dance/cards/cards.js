42377aee4790d4607dfa0f825457f3e1ffe78a5c
harshdhonchak <harshdhounchak4@gmail.com>
2021-06-20 08:09:52 +0530

    Dance Section

---
+++ components/Dance/cards/cards.js
@@ -0,0 +1,31 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Card } from 'react-bootstrap';
+import styles from './cards.module.css';
+import * as Icon from 'react-bootstrap-icons';
+
+
+
+
+
+function Cards(props) {
+    return (
+        <>
+
+            <Card className={styles.main_card}
+
+            >
+
+                <Card.Body>
+                    <footer ><h1 className={styles.card_content}>{props.classes}</h1>
+                        <p className={styles.wishlist}>ADD TO WISHLIST {' '}<Icon.BookmarkFill className={styles.bookmark} /></p>
+
+                    </footer>
+
+
+
+                </Card.Body>
+            </Card>
+        </>
+    )
+}
+export default Cards;
---
+++ components/Event/Event.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Styles from './Event.module.css';
+function Events() {
+    return (
+        <div>
+            <h1>Events</h1>
+        </div>
+    )
+}
+
+export default Events
